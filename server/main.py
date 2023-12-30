f739a3b2b1f8858fbfafa236a5df8a4e7aa8e8c7
Gorkem Ridvan <76165528+sugrado@users.noreply.github.com>
2023-12-31 00:36:13 +0300

    get district info by lat,long and district name endpoints

--- server/main.py
+++ server/main.py
@@ -1,32 +1,46 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.params import Query
+from shapely.geometry import Point, shape
+import json
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 
-# FastAPI uygulamasını oluşturma
 app = FastAPI()
 
-def recommend_products(location):
-    if location == 'bozkir':
-        return {
-            'aciklama': 'bu mekan cok iyi bir mekandir',
-            'onerilen urunler': ['elma', 'armut', 'mal tunahan']
-        }
+geojson_file_path = "C:\\Users\\sugrado\\Desktop\\datathon\\server\\konya_mahalleler.geojson"
+
+def get_neighborhood_from_coords(lat: float = Query(None, description="Latitude of the location"),
+                     long: float = Query(None, description="Longitude of the location")):
+    with open(geojson_file_path, encoding="UTF-8") as f:
+        data = json.load(f)
+
+    point = Point(long, lat)
+
+    for feature in data['features']:
+        polygon = shape(feature['geometry'])
+
+        if point.within(polygon):
+            neighborhood_name = feature['properties']['ADI_NUMARA']
+            return neighborhood_name
+
+    raise HTTPException(status_code=404, detail="No neighborhood found for the given coordinates")
+
+def get_neighborhood_info(neighborhood_name: str ):
+    res = {"description": f"{neighborhood_name} cok iyi bir mekandir", "suggested_products": ['elma', 'armut', 'üzüm']}
+    return JSONResponse(content=jsonable_encoder(res))
+
+
+@app.get("/get_neighborhood_info")
+async def read_item(lat: float | None = None, long: float| None = None, neighborhood_name: str| None = None):
+    if neighborhood_name:
+        res = get_neighborhood_info(neighborhood_name)
+        return res
+    elif lat and long:
+        res = get_neighborhood_info(get_neighborhood_from_coords(lat=lat, long=long))
+        return res
     else:
-        return {
-            'aciklama': 'bu mekan cok kotu bir mekandir',
-            'onerilen urunler': ['cilek', 'armut tunahan']
-        }
+        raise HTTPException(status_code=404, detail="No neighborhood found for the given coordinates")
 
-
-# Basit bir GET endpoint'i
-@app.get("/")
-def read_root():
-    return recommend_products('bozkir')
-
-# Başka bir GET endpoint'i
-@app.get("/hello/{name}")
-def say_hello(name: str):
-    return {"message": f"Merhaba, {name}!"}
-
-# Uygulamayı çalıştırma
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run(app, host="0.0.0.0", port=8000)
